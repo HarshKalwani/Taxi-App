25bc67c4b4c412e37890179d22d340b58e23e19e
HarshKalwani <harshkalwani48@gmail.com>
2024-12-05 01:59:58 +0530

    captain login,logout get_profile done with testing

--- Backend/middlewares/auth.middlewares.js
+++ Backend/middlewares/auth.middlewares.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 module.exports.authUser = async(req,res,next)=>{
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
@@ -8,7 +10,7 @@ module.exports.authUser = async(req,res,next)=>{
         return res.status(401).json({message:"Unauthorized"})
     }
 
-    const isBlackListed = await userModel.findOne({token : token});
+    const isBlackListed = await blacklistTokenModel.findOne({token : token});
     if(isBlackListed){
         return res.status(401).json({message:"Unauthorized"});
     }
@@ -17,6 +19,27 @@ module.exports.authUser = async(req,res,next)=>{
         const decode = jwt.verify(token , process.env.JWT_SECRET);
         const user = await userModel.findById(decode._id)
         req.user = user;
+        return next();
+    } catch (error) {
+        return res.status(401).json({message:"Unauthorized"})
+    }
+}
+
+module.exports.authCaptain = async(req,res,next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"})
+    }
+
+    const isBlackListed = await blacklistTokenModel.findOne({token : token});
+    if(isBlackListed){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+
+    try {
+        const decode = jwt.verify(token , process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decode._id)
+        req.captain = captain;
         return next();
     } catch (error) {
         return res.status(401).json({message:"Unauthorized"})
